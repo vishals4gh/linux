e48e3ad93ed23e9f2f79c35cd6d5b12b09982de3
Vishal Annapurve <vannapurve@google.com>
2022-08-30 21:59:35 +0000

    selftests: kvm: Refactor testing logic for private memory
    Move all of the logic to execute memory conversion tests into library to
    allow sharing the logic between normal non-confidential VMs and SEV VMs.
    
    Signed-off-by: Vishal Annapurve <vannapurve@google.com>

---
+++ tools/testing/selftests/kvm/include/x86_64/private_mem_test_helper.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (C) 2022, Google LLC.
+ */
+
+#ifndef SELFTEST_KVM_PRIVATE_MEM_TEST_HELPER_H
+#define SELFTEST_KVM_PRIVATE_MEM_TEST_HELPER_H
+
+void execute_memory_conversion_tests(void);
+
+void execute_sev_memory_conversion_tests(void);
+
+#endif  // SELFTEST_KVM_PRIVATE_MEM_TEST_HELPER_H
