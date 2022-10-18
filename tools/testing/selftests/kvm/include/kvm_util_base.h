6a5da69231f35da742423a707778ae64d49d890d
Peter Gonda <pgonda@google.com>
2022-10-18 13:58:40 -0700

    KVM: selftests: add hooks for managing protected guest memory
    Add kvm_vm.protected metadata. Protected VMs memory, potentially
    register and other state may not be accessible to KVM. This combined
    with a new protected_phy_pages bitmap will allow the selftests to check
    if a given pages is accessible.
    
    Originally-by: Michael Roth <michael.roth@amd.com>
    Signed-off-by: Peter Gonda <pgonda@google.com>

--- tools/testing/selftests/kvm/include/kvm_util_base.h
+++ tools/testing/selftests/kvm/include/kvm_util_base.h
@@ -36,6 +36,7 @@ struct userspace_mem_region {
 		struct kvm_userspace_memory_region_ext region_ext;
 	};
 	struct sparsebit *unused_phy_pages;
+	struct sparsebit *protected_phy_pages;
 	int fd;
 	off_t offset;
 	void *host_mem;
@@ -92,6 +93,9 @@ struct kvm_vm {
 	vm_vaddr_t idt;
 	vm_vaddr_t handlers;
 	uint32_t dirty_ring_size;
+
+	/* VM protection enabled: SEV, etc*/
+	bool protected;
 
 	/* Cache of information for binary stats interface */
 	int stats_fd;
@@ -646,9 +650,15 @@ const char *exit_reason_str(unsigned int exit_reason);
 
 vm_paddr_t vm_phy_page_alloc(struct kvm_vm *vm, vm_paddr_t paddr_min,
 			     uint32_t memslot);
-vm_paddr_t vm_phy_pages_alloc(struct kvm_vm *vm, size_t num,
-			      vm_paddr_t paddr_min, uint32_t memslot);
+vm_paddr_t _vm_phy_pages_alloc(struct kvm_vm *vm, size_t num,
+			      vm_paddr_t paddr_min, uint32_t memslot, bool protected);
 vm_paddr_t vm_alloc_page_table(struct kvm_vm *vm);
+
+static inline vm_paddr_t vm_phy_pages_alloc(struct kvm_vm *vm, size_t num,
+					    vm_paddr_t paddr_min, uint32_t memslot)
+{
+	return _vm_phy_pages_alloc(vm, num, paddr_min, memslot, vm->protected);
+}
 
 /*
  * ____vm_create() does KVM_CREATE_VM and little else.  __vm_create() also
